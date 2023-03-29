b26f45ff0c7be89458f3aef6bf4475fdc7fe858c
farheen tahir <farheentahir200@gmail.com>
2023-03-29 12:56:20 +0500

    map donor recipient

--- src/components/MapConfiguration.js
+++ src/components/MapConfiguration.js
@@ -1,4 +1,4 @@
-const GOOGLE_MAPS_API_KEY="AIzaSyAzOUqoqNhMXMKQwxxrkc78F4TFOJj39fE";
+const GOOGLE_MAPS_API_KEY=process.env.GOOGLE_MAPS_API_KEY;
 export const mapOptions={
     googleMapApiKey: GOOGLE_MAPS_API_KEY
 };
